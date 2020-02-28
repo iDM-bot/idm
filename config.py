463d2bc566bb396bced48fa3107060a72a48a02d
James C <29265756+ottter@users.noreply.github.com>
2020-02-28 01:23:09 -0500

    Update config.py

--- config.py
+++ config.py
@@ -7,9 +7,9 @@ server_time = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H
 bot_description = 'iDM for Discord'
 discord_game_played = f'Duel Arena'
 
-discord_token = 'NjE3ODM2Njg0NTExNDEyMjQ5.Xlc_kw.9BlGFSiPfkPA9qDGm_7lKYgu0c0'
+discord_token = os.environ['IDM_DISCORD_TOKEN']
 
-db_connection_string = f'mongodb+srv://idm:fip9xZm6kJ1w9O2i@idm-koiuh.gcp.mongodb.net/test'
+db_connection_string = f'mongodb+srv://{os.environ["IDM_MONGO_PASSWORD"]}@idm-koiuh.gcp.mongodb.net/test'
 
 bot_name = f'iDM'
 
